19a4f7da12b61110bdcee80eb1094480d2e354ad
NigelOmondi <nigelomondi62@gmail.com>
2023-11-29 19:13:04 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ];
-
-  return Promise.allSettled(promises).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason.message,
+  ]).then((values) => values.map((value) => ({
+    status: value.status,
+    value: value.status === 'fulfilled' ? value.value : String(value.reason),
   })));
 }
