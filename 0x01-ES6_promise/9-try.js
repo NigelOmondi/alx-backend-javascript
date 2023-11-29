f3b0a0bdc2cad054ed09dba833eb42e4b3098c5e
NigelOmondi <nigelomondi62@gmail.com>
2023-11-29 19:26:08 +0300

    Create and return an array named queue

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,15 +1,14 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-  
-    try {
-      const result = mathFunction();
-      queue.push(result);
-    } catch (error) {
-      queue.push(new Error(error.message)); // Wrap the error message in an Error object
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-  
-    return queue;
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(new Error(error.message)); // Wrap the error message in an Error object
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  
+
+  return queue;
+}
