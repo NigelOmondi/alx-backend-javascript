c54a1242cf86cfab3fa4f6d50249a641ed754558
NigelOmondi <nigelomondi62@gmail.com>
2023-12-06 21:06:04 +0300

    Advanced types part 1

---
+++ 0x04-TypeScript/task_2/js/main.js
@@ -0,0 +1,43 @@
+// Class implementing DirectorInterface
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return 'Working from home';
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return 'Getting a coffee break';
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return 'Getting to director tasks';
+    };
+    return Director;
+}());
+// Class implementing TeacherInterface
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return 'Cannot work from home';
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return 'Cannot have a break';
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return 'Getting to work';
+    };
+    return Teacher;
+}());
+// Function createEmployee
+function createEmployee(salary) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+}
+// Test cases
+console.log(createEmployee(200)); // Output: Teacher
+console.log(createEmployee(1000)); // Output: Director
+console.log(createEmployee('$500')); // Output: Director
