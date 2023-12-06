c57367e0734c69203ff699f8495493eddf311a05
NigelOmondi <nigelomondi62@gmail.com>
2023-12-06 21:16:55 +0300

    String literals

--- 0x04-TypeScript/task_2/js/main.js
+++ 0x04-TypeScript/task_2/js/main.js
@@ -1,43 +1,16 @@
-// Class implementing DirectorInterface
-var Director = /** @class */ (function () {
-    function Director() {
+// Function teachClass
+function teachClass(todayClass) {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
     }
-    Director.prototype.workFromHome = function () {
-        return 'Working from home';
-    };
-    Director.prototype.getCoffeeBreak = function () {
-        return 'Getting a coffee break';
-    };
-    Director.prototype.workDirectorTasks = function () {
-        return 'Getting to director tasks';
-    };
-    return Director;
-}());
-// Class implementing TeacherInterface
-var Teacher = /** @class */ (function () {
-    function Teacher() {
-    }
-    Teacher.prototype.workFromHome = function () {
-        return 'Cannot work from home';
-    };
-    Teacher.prototype.getCoffeeBreak = function () {
-        return 'Cannot have a break';
-    };
-    Teacher.prototype.workTeacherTasks = function () {
-        return 'Getting to work';
-    };
-    return Teacher;
-}());
-// Function createEmployee
-function createEmployee(salary) {
-    if (typeof salary === 'number' && salary < 500) {
-        return new Teacher();
+    else if (todayClass === 'History') {
+        return 'Teaching History';
     }
     else {
-        return new Director();
+        // Handle other cases if necessary
+        return 'Unknown subject';
     }
 }
 // Test cases
-console.log(createEmployee(200)); // Output: Teacher
-console.log(createEmployee(1000)); // Output: Director
-console.log(createEmployee('$500')); // Output: Director
+console.log(teachClass('Math')); // Output: Teaching Math
+console.log(teachClass('History')); // Output: Teaching History
--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,58 +1,18 @@
-// Interface for Director
-interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
+// String literal type
+type Subjects = 'Math' | 'History';
+
+// Function teachClass
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    // Handle other cases if necessary
+    return 'Unknown subject';
   }
-  
-  // Interface for Teacher
-  interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
-  }
-  
-  // Class implementing DirectorInterface
-  class Director implements DirectorInterface {
-    workFromHome(): string {
-      return 'Working from home';
-    }
-  
-    getCoffeeBreak(): string {
-      return 'Getting a coffee break';
-    }
-  
-    workDirectorTasks(): string {
-      return 'Getting to director tasks';
-    }
-  }
-  
-  // Class implementing TeacherInterface
-  class Teacher implements TeacherInterface {
-    workFromHome(): string {
-      return 'Cannot work from home';
-    }
-  
-    getCoffeeBreak(): string {
-      return 'Cannot have a break';
-    }
-  
-    workTeacherTasks(): string {
-      return 'Getting to work';
-    }
-  }
-  
-  // Function createEmployee
-  function createEmployee(salary: number | string): Director | Teacher {
-    if (typeof salary === 'number' && salary < 500) {
-      return new Teacher();
-    } else {
-      return new Director();
-    }
-  }
-  
-  // Test cases
-  console.log(createEmployee(200)); // Output: Teacher
-  console.log(createEmployee(1000)); // Output: Director
-  console.log(createEmployee('$500')); // Output: Director
-  
+}
+
+// Test cases
+console.log(teachClass('Math')); // Output: Teaching Math
+console.log(teachClass('History')); // Output: Teaching History
